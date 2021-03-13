7c8ec54ccb504e962ac7815f9d7369edcba050dc
David Ryder <67267592+david-ryder@users.noreply.github.com>
2021-03-12 19:56:49 -0500

    Update main.py
    added addition functionality

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def on_message(message):
       if message.content[x] == '+':
         plus = x
 
-    if plus == 0:
+    if plus == 0: # if no + in input, default to length
       plus = length
 
     for x in range(length): # Gets number of characters before d
@@ -45,33 +45,44 @@ async def on_message(message):
     
     num_rolls = ''
 
-    for x in range(1,pre+1):
+    for x in range(1,pre+1): # get num_rolls
       num_rolls += message.content[x]
     
     post = length - pre - 2
 
     die_type = ''
 
-    for x in range(length - post, plus):
+    for x in range(length - post, plus): # get die_type
       die_type += message.content[x]
+
+    addition = ''
+
+    if plus != length: # get value to add to total later
+      for x in range(plus, length):
+        addition += message.content[x]
+
+    if addition:
+      addition = int(addition)
+    else:
+      addition = 0
 
     arr = []
 
     for x in range(int(num_rolls)): # execute die roll and store in array
-      arr.append(roll(int(die_type)))
+      arr.append(roll(int(die_type)) + addition)
 
-    query = message.content.replace('.','')
+    query = message.content.replace('.','') # delete the command value from the input string
 
     final_message = message.author.mention + '\n***Rolling ' + query + ':***   ('
 
     for x in range(int(num_rolls)):
-      if int(die_type) == 20 and arr[x] == 20:
+      if int(die_type) == 20 and arr[x] - addition == 20:
         final_message += '***'
       if x == int(num_rolls) - 1:
         final_message += str(arr[x]) + ')'
       else:
         final_message += str(arr[x]) + ', '
-      if int(die_type) == 20 and arr[x] == 20:
+      if int(die_type) == 20 and arr[x] - addition == 20:
         final_message += '***'
 
     total = 0
