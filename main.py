afc247afe03ef4f6d1c7dc1e135ccbe7d94ab717
David Ryder <67267592+david-ryder@users.noreply.github.com>
2021-03-12 19:43:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 
+
 def roll(a):
-  num = random.randint(1,a)
+  num = random.randint(1, a)
   return num
 
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -25,6 +27,15 @@ async def on_message(message):
     length = len(message.content)
 
     pre = -1 # num characters before d
+
+    plus = 0
+
+    for x in range(length): # get index value of +, if there is one
+      if message.content[x] == '+':
+        plus = x
+
+    if plus == 0:
+      plus = length
 
     for x in range(length): # Gets number of characters before d
       if message.content[x] != 'd':
@@ -41,25 +52,27 @@ async def on_message(message):
 
     die_type = ''
 
-    for x in range(length - post, length):
+    for x in range(length - post, plus):
       die_type += message.content[x]
 
     arr = []
 
-    for x in range(int(num_rolls)): # execute die roll
+    for x in range(int(num_rolls)): # execute die roll and store in array
       arr.append(roll(int(die_type)))
 
     query = message.content.replace('.','')
 
-    
-
     final_message = message.author.mention + '\n***Rolling ' + query + ':***   ('
 
     for x in range(int(num_rolls)):
+      if int(die_type) == 20 and arr[x] == 20:
+        final_message += '***'
       if x == int(num_rolls) - 1:
         final_message += str(arr[x]) + ')'
       else:
         final_message += str(arr[x]) + ', '
+      if int(die_type) == 20 and arr[x] == 20:
+        final_message += '***'
 
     total = 0
 
@@ -72,5 +85,6 @@ async def on_message(message):
 
 
 keep_alive()
+
 
 client.run(os.getenv('TOKEN'))
